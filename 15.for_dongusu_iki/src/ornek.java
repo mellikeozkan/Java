f088ad44bcec661c69563c71c28edd12205ffd6a
Melike Özkan <115733605+mellikeozkan@users.noreply.github.com>
2023-12-14 16:30:21 +0300

    Add files via upload

---
+++ 15.for_dongusu_iki/src/ornek.java
@@ -0,0 +1,26 @@
+
+public class ornek {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  // 1 DEN 100 E KADAR OLAN TEK SAYILARIN TOPLAMI 
+	 int toplam =0;
+	
+	  for (int i=1; i<=100; i++) {
+		  
+		  if(i%2==1)
+		  {
+			  toplam+=i;
+		  }
+		  
+		  
+	  }
+	
+	   System.out.println("Sonuç= "+toplam);
+	
+	}
+	
+	
+	
+
+}
---
+++ 16.while_dongusu/src/while_dongusu.java
@@ -0,0 +1,19 @@
+
+public class while_dongusu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		 int i =1;
+		 while (i<=10)
+			 
+		 {
+			 System.out.println(i);
+			 i++;
+		 }
+	}
+	
+
+}
